318206376d851ff7e7c79577abe9a23aff770289
SAHIL <87164152+sahil8421@users.noreply.github.com>
2022-08-06 17:39:14 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,173 @@
+
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('sawagat1'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets5.lottiefiles.com/packages/lf20_rbciw8tr.json' // lottie file path
+  })
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('sawagat2'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets2.lottiefiles.com/packages/lf20_fJ7CVd.json' // lottie file path
+  })
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('sawagat3'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets2.lottiefiles.com/packages/lf20_fJ7CVd.json' // lottie file path
+  })
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('anim1Container'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets2.lottiefiles.com/packages/lf20_q06flcaz.json' // lottie file path
+  })
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('anim2Container'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets2.lottiefiles.com/packages/lf20_q06flcaz.json' // lottie file path
+  })
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('anim3Container'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets2.lottiefiles.com/packages/lf20_mvtopocg.json' // lottie file path
+  })
+ 
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('anim4Container'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets2.lottiefiles.com/packages/lf20_mvtopocg.json' // lottie file path
+  })
+ 
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('anim5Container'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets2.lottiefiles.com/packages/lf20_opb3pm28.json' // lottie file path
+  })
+ 
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('anim6Container'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets2.lottiefiles.com/packages/lf20_opb3pm28.json' // lottie file path
+  })
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('anim7Container'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '  https://assets8.lottiefiles.com/packages/lf20_kkxevalq.json' // lottie file path
+  })
+ 
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('anim8Container'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '  https://assets8.lottiefiles.com/packages/lf20_kkxevalq.json' // lottie file path
+  })
+
+//   Ending
+
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('end1'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets10.lottiefiles.com/packages/lf20_jEMHbp.json' // lottie file path
+  })
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('end2'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets10.lottiefiles.com/packages/lf20_5hufvwkz.json' // lottie file path
+  })
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('end3'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '  https://assets10.lottiefiles.com/private_files/lf30_vzqrg39e.json' // lottie file path
+  })
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('end4'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: ' https://assets10.lottiefiles.com/packages/lf20_bx1zecgg.json' // lottie file path
+  })
+ 
+
+ 
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('end5'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets10.lottiefiles.com/packages/lf20_jEMHbp.json' // lottie file path
+  })
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('end6'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets10.lottiefiles.com/packages/lf20_5hufvwkz.json' // lottie file path
+  })
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('end7'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '  https://assets10.lottiefiles.com/private_files/lf30_vzqrg39e.json' // lottie file path
+  })
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('end8'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: ' https://assets10.lottiefiles.com/packages/lf20_bx1zecgg.json' // lottie file path
+  })
+  
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('end9'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets10.lottiefiles.com/packages/lf20_jEMHbp.json' // lottie file path
+  })
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('end10'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets10.lottiefiles.com/packages/lf20_5hufvwkz.json' // lottie file path
+  })
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('end11'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '  https://assets10.lottiefiles.com/private_files/lf30_vzqrg39e.json' // lottie file path
+  })
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('end12'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: ' https://assets10.lottiefiles.com/packages/lf20_bx1zecgg.json' // lottie file path
+  })
